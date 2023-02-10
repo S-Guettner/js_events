be5f3a9a927fde9d3af348d52cfc52d9a651f942
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-10 15:05:23 +0100

    finish

--- 3_1/main.js
+++ 3_1/main.js
@@ -3,6 +3,7 @@ const rot = document.querySelector("#rot")
 const grun = document.querySelector("#grun")
 const blau = document.querySelector("#blau")
 const counterOutput = document.querySelector("#counterOutput")
+const hexOutput = document.querySelector("#hexOutput")
 
 let counter = 0
 
@@ -14,4 +15,16 @@ const changeBackground = (color) => {
 
 const changeHandle = () => {
     body.style.backgroundColor=`rgb(${rot.value},${grun.value},${blau.value})`
+    r = (Number(rot.value)).toString(16)
+    g = (Number(grun.value)).toString(16)
+    b = (Number(blau.value)).toString(16)
+
+    if (r.length == 1)
+    r = "0" + r
+    if (g.length == 1)
+    g = "0" + g
+    if (b.length == 1)
+    b = "0" + b
+    hexOutput.textContent = `#${r}${g}${b}`
 }
+
